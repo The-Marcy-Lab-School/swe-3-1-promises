d0e3ab0546d71e1b767727f6cc1bbbba700d9876
benspector-mls <ben@marcylabschool.org>
2024-12-07 11:31:55 -0500

    adding comments to modify

--- src/modify.js
+++ src/modify.js
@@ -7,19 +7,24 @@ const convertColorsArrToRGB = ([color1, color2, color3]) => {
   return `rgb(${color1}, ${color2}, ${color3})`;
 };
 
-const getRandomBytes = () => new Promise((resolve, reject) => {
-  crypto.randomFill(new Uint8Array(3), (err, buffer) => {
-    if (err) return reject(err);
-    const colorsArr = [...buffer]
-    // Focus here!
-    // An array like this will be resolved: [123, 211, 105]
-    resolve(colorsArr);
-  });
-});
+const getRandomBytes = () => {
+  return new Promise((resolve, reject) => {
+    // we're basically turning this code below into a Promise
+    crypto.randomFill(new Uint8Array(3), (err, buffer) => {
+      if (err) return reject(err);
+      // Focus here!
+      // An array like this will be resolved: [123, 211, 105]
+      const colorsArr = [...buffer]
+      resolve(colorsArr);
+    });
+  })
+};
 
 // Hints: 
 // - understand what getRandomBytes() resolves to
+// - take the resolved colorsArr, turn it into an rgb color, and add it to colors
 // - understand how to chain together getRandomBytes
+// - make sure to return colors at the end
 const return4RandomColors = () => {
   const colors = [];
   return getRandomBytes()
